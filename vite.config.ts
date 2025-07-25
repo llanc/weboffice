6be0b2dde0c3ff395c5dc273a93e33a72c7db038
Langel <liulancong@inspur.com>
2025-07-25 15:58:51 +0800

    cf page with r2

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: '/document',
   publicDir: 'public',
   resolve: {
     alias: {
